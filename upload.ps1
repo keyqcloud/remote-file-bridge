b32e598f3a0c0feb2622febced07a72922594d2e
Kenneth Hough <kenneth@keyqcloud.com>
2024-04-08 16:44:32 -0500

    Create powershell version of file upload

---
+++ upload.ps1
@@ -0,0 +1,35 @@
+param (
+    [Parameter(Mandatory=$true)][string]$FilePath,
+    [Parameter(Mandatory=$true)][string]$Token
+)
+
+$Endpoint = "https://pb30c2aqsh.execute-api.us-east-2.amazonaws.com/prod/upload"
+
+if (-Not (Test-Path $FilePath)) {
+    Write-Error "File $FilePath does not exist."
+    exit
+}
+
+# Simple MIME type determination (you might want to expand this based on your needs)
+$extension = [System.IO.Path]::GetExtension($FilePath).ToLower()
+$MIMEType = switch ($extension) {
+    '.txt' { 'text/plain' }
+    '.jpg' { 'image/jpeg' }
+    '.jpeg' { 'image/jpeg' }
+    '.png' { 'image/png' }
+    Default { 'application/octet-stream' } # Default generic binary stream MIME type
+}
+
+$FileName = [System.IO.Path]::GetFileName($FilePath)
+
+$Headers = @{
+    "Content-Type" = $MIMEType
+    "security-token" = $Token
+    "file-name" = $FileName
+}
+
+$Content = Get-Content $FilePath -Raw -Encoding Byte
+
+$response = Invoke-RestMethod -Uri $Endpoint -Method Post -Headers $Headers -Body $Content -TimeoutSec 300
+
+Write-Output $response
